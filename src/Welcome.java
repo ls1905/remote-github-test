796dbf77d4cc4e78e2887579b0ca016a888315ce
ls1905 <ls1905@163.com>
2023-12-29 15:55:53 +0800

    修改了文件

---
+++ src/Welcome.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Welcome
+ * @Description:
+ * @Author Administrator
+ * @Date 2023/12/29 15:51
+ * @Version 1.0
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("你好，git");
+    }
+}
